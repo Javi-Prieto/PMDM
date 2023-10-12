fe1cb5eed89987b3917e89a8c71fad41d5609b97
Javier Prieto <jpojaviprior@gmail.com>
2023-10-12 19:01:37 +0200

    Finished the exercises of car and restaurant

--- T2/angular/ejercicios_componentes/src/app/app.module.ts
+++ T2/angular/ejercicios_componentes/src/app/app.module.ts
@@ -5,16 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { CarCardComponent } from './car-card/car-card.component';
+import { RestaurantCardComponent } from './restaurant-card/restaurant-card.component';
+import { NgbdRatingBasic } from './rating/rating.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CarCardComponent
+    CarCardComponent,
+    RestaurantCardComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    NgbdRatingBasic
   ],
   providers: [],
   bootstrap: [AppComponent]
