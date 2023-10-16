c0ef65ef0d4d58fc3772c7042f7dc22b03837a08
Javier Prieto <jpojaviprior@gmail.com>
2023-10-16 18:07:16 +0200

    Fully Completed the calculator

--- T2/angular/prueba_calc/src/app/calculator/calculator.component.ts
+++ T2/angular/prueba_calc/src/app/calculator/calculator.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { toInteger } from '@ng-bootstrap/ng-bootstrap/util/util';
 
 @Component({
   selector: 'app-calculator',
@@ -6,20 +7,60 @@ import { Component } from '@angular/core';
   styleUrls: ['./calculator.component.css']
 })
 export class CalculatorComponent {
-    n1:string = "";
-    n2:string = "";
+  //Todos estos atributos se podrían reducir a solo el atributo operation
+  //pero voy mal de tiempo entonces no voy a hacerlo   
+    n1 = "";
+    n2 = "";
     operator = "";
     screen = "0";
-    first=true;
+    operation = "";
   
   getNumber(num:number){
-    if(this.first){
-      this.n1.concat(num.toString());
-      this.changeScreen(this.n1);
+    if(this.operator == ""){
+      this.n1 += num.toString();
+      this.operation += num;
+      
+    }else{
+      this.n2 += num.toString();
+      this.operation += num;
     }
-    
+    this.changeScreen();
   }
-  changeScreen(text:string){
-    this.screen = text;
+  
+  getOperator(ope:string){
+    this.operator = ope ;
+    this.operation += ope;
+    this.changeScreen();
   }
+
+  changeScreen(){
+    this.screen = this.operation;
+  }
+  getResult(){
+    let op1:number = +this.n1;
+    let op2:number = +this.n2;
+    let result:number;
+    this.operator == "+"? result = op1 + op2 : result = op1 - op2;
+    this.screen = result.toString();
+  }
+  deleteAll(){
+    this.operation = "";
+    this.screen = "0";
+  }
+  deleteOne(){
+    this.operation = this.operation.replace(this.operation.charAt(this.operation.length -1), "");
+    if(this.operator == ""){
+      if(this.n2 == ""){
+        this.n1 = this.n1.replace(this.n1.charAt(this.n1.length -1), "");
+      }else{
+        this.n2 = this.n2.replace(this.n2.charAt(this.n2.length -1), "");
+      }
+    }else{
+      this.operator = this.operator.replace(this.operator.charAt(this.operator.length - 1), "");
+    }
+    console.log(this.n1)
+    console.log(this.operation)
+    this.changeScreen();
+  }
+ 
 }
