a0e7ee662bcbeb073b2ddf83102850b8d28ee571
Javier Prieto <jpojaviprior@gmail.com>
2023-10-16 13:33:47 +0200

    Calculator initialized

--- T2/angular/prueba_calc/src/app/calculator/calculator.component.ts
+++ T2/angular/prueba_calc/src/app/calculator/calculator.component.ts
@@ -6,6 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./calculator.component.css']
 })
 export class CalculatorComponent {
-    private n1:number = 0;
-    private n2:number = 0;
+    n1:string = "";
+    n2:string = "";
+    operator = "";
+    screen = "0";
+    first=true;
+  
+  getNumber(num:number){
+    if(this.first){
+      this.n1.concat(num.toString());
+      this.changeScreen(this.n1);
+    }
+    
+  }
+  changeScreen(text:string){
+    this.screen = text;
+  }
 }
