6358ee5e0ab03b233fdeaf5f4780207d117d3341
Javier Prieto <jpojaviprior@gmail.com>
2023-10-19 14:23:52 +0200

    Checkbox left

---
+++ T2/angular/angular-listado-alumnos/src/app/models/table.interface.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+import {MatTableModule} from '@angular/material/table';
+
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
