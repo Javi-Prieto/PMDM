0c3292fa9b4a4b8c7e1e214edeea1d99a11cf27a
Javier Prieto <jpojaviprior@gmail.com>
2023-10-25 13:36:10 +0200

    Added the example of get petition with httpclient

---
+++ T2/angular/peticiones_api/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
+import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { PokemonListComponent } from './components/pokemon-list/pokemon-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PokemonListComponent
+  ],
+  imports: [
+    BrowserModule,
+    NgbModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
