b154a05e4f934f9c8fe324af485e959f8bec1a50
Javier Prieto <jpojaviprior@gmail.com>
2024-01-09 22:12:05 +0100

    done the exercise with the login not functional login but yes printed

--- Flutter/my_first_login/lib/home_screen.dart
+++ Flutter/my_first_login/lib/home_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -12,23 +14,90 @@ class HomeScreen extends StatelessWidget {
       body: Container(
         alignment: Alignment.center,
         color: Colors.red,
-        child: const Column(
+        child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            Column(
+            const Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Image(image: NetworkImage('https://miravosblog.files.wordpress.com/2014/10/path-logo.jpg')),
-                Text('Beautiful, Private & Sharing')
+                Image(
+                    image: NetworkImage(
+                        'https://miravosblog.files.wordpress.com/2014/10/path-logo.jpg')),
+                Text('Beautiful, Private & Sharing',
+                    style: TextStyle(color: Colors.white))
               ],
             ),
             Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              mainAxisSize: MainAxisSize.max,
               children: [
-                Text('Sign Up'),
-                Text('Already have a Path account?'),
-                Text('Log In'),
-                Text('By using Path, you agree to Paths Terms of Use and Privacy Policy'),
+                Padding(
+                  padding: const EdgeInsets.all(4.0),
+                  child: FilledButton(
+                    onPressed: () {},
+                    style: FilledButton.styleFrom(
+                        backgroundColor: Colors.white,
+                        fixedSize: const Size(200, 75),
+                        shape: const BeveledRectangleBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.elliptical(5, 5)))),
+                    child: const Text(
+                      'Sign Up',
+                      style: TextStyle(
+                          color: Colors.red,
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 50,
+                ),
+                const Text(
+                  'Already have a Path account?',
+                  style: TextStyle(color: Colors.grey),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(4.0),
+                  child: OutlinedButton(
+                    onPressed: () {},
+                    style: OutlinedButton.styleFrom(
+                        fixedSize: const Size(200, 75),
+                        shape: const BeveledRectangleBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.elliptical(5, 5)))),
+                    child: const Text(
+                      'Log In',
+                      style: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 50,
+                ),
+                RichText(
+                    text: const TextSpan(
+                  text: 'By using Path, you agree to Paths ',
+                  style: TextStyle(color: Colors.grey),
+                  children: <TextSpan>[
+                    TextSpan(
+                        text: 'Terms of User ',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                            decoration: TextDecoration.underline)),
+                    TextSpan(text: ' and '),
+                    TextSpan(
+                        text: 'Privacy Policy',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                            decoration: TextDecoration.underline)),
+                  ],
+                )),
               ],
             )
           ],
