592ff07808a0b9b725ebd157215489ef802a9b9d
Javier Prieto <jpojaviprior@gmail.com>
2024-01-11 23:26:20 +0100

    finalized

--- Flutter/flutter_lateral_scrollable/lib/screen/home_screen.dart
+++ Flutter/flutter_lateral_scrollable/lib/screen/home_screen.dart
@@ -7,50 +7,69 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Actors and Actresses')),
+      appBar: AppBar(
+          title: const Text(
+            'Actors and Actresses',
+            style: TextStyle(fontWeight: FontWeight.bold),
+            textAlign: TextAlign.center,
+          ),
+          elevation: 1,
+          leading: const Icon(
+            Icons.accessible_rounded,
+            size: 40,
+          )),
       body: ListView(
         scrollDirection: Axis.horizontal,
         children: const [
           ActorItem(
-              imageUrl:
-                  'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/Sydney_Sweeney_2019_by_Glenn_Francis.jpg/220px-Sydney_Sweeney_2019_by_Glenn_Francis.jpg',
-              name: 'Sidney'),
+            imageUrl:
+                'https://upload.wikimedia.org/wikipedia/commons/e/e8/Chris_Hemsworth_by_Gage_Skidmore_2_%28cropped%29.jpg',
+            name: 'Chris',
+            firstName: 'Hemsworth',
+          ),
           ActorItem(
-              imageUrl:
-                  'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/Sydney_Sweeney_2019_by_Glenn_Francis.jpg/220px-Sydney_Sweeney_2019_by_Glenn_Francis.jpg',
-              name: 'Sidney'),
+            imageUrl:
+                'https://m.media-amazon.com/images/M/MV5BMjI4NjM1NDkyN15BMl5BanBnXkFtZTgwODgyNTY1MjE@._V1_.jpg',
+            name: 'Enma',
+            firstName: 'Stones',
+          ),
           ActorItem(
-              imageUrl:
-                  'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/Sydney_Sweeney_2019_by_Glenn_Francis.jpg/220px-Sydney_Sweeney_2019_by_Glenn_Francis.jpg',
-              name: 'Sidney'),
+            imageUrl:
+                'https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/Michael_B._Jordan_Cannes_2018.jpg/1200px-Michael_B._Jordan_Cannes_2018.jpg',
+            name: 'Michael B.',
+            firstName: 'Jordan',
+          ),
           ActorItem(
-              imageUrl:
-                  'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/Sydney_Sweeney_2019_by_Glenn_Francis.jpg/220px-Sydney_Sweeney_2019_by_Glenn_Francis.jpg',
-              name: 'Sidney')
+            imageUrl:
+                'https://upload.wikimedia.org/wikipedia/commons/thumb/c/ce/Robbie_at_2019_Cannes_%28cropped%29.jpg/220px-Robbie_at_2019_Cannes_%28cropped%29.jpg',
+            name: 'Margot',
+            firstName: 'Robbie',
+          ),
         ],
       ),
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
-            icon: Icon(Icons.home, color: Colors.black),
+            icon: Icon(Icons.home),
             label: 'Home',
           ),
           BottomNavigationBarItem(
               icon: Icon(
                 Icons.camera_alt,
-                color: Colors.black,
               ),
               label: 'Booking'),
           BottomNavigationBarItem(
-            icon: Icon(Icons.list, color: Colors.black),
+            icon: Icon(Icons.list),
             label: 'List',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.person, color: Colors.black),
+            icon: Icon(Icons.person),
             label: 'Profile',
           )
         ],
         backgroundColor: Colors.grey.shade500,
+        selectedItemColor: Colors.grey.shade800,
+        unselectedItemColor: Colors.black,
       ),
     );
   }
--- Flutter/flutter_lateral_scrollable/lib/widget/actor_item.dart
+++ Flutter/flutter_lateral_scrollable/lib/widget/actor_item.dart
@@ -1,17 +1,23 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class ActorItem extends StatelessWidget {
   final String imageUrl;
-  final String name;
-  const ActorItem({super.key, required this.imageUrl, required this.name});
+  final String name, firstName;
+  const ActorItem(
+      {super.key,
+      required this.imageUrl,
+      required this.firstName,
+      required this.name});
 
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: const EdgeInsets.all(20),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           ClipRRect(
             borderRadius: BorderRadius.circular(500),
@@ -20,11 +26,18 @@ class ActorItem extends StatelessWidget {
               alignment: Alignment.center,
               fit: BoxFit.cover,
               height: 600,
+              width: 300,
             ),
           ),
           Text(
             name,
-            style: const TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
+            style: GoogleFonts.ptSansNarrow(fontSize: 50),
+            textAlign: TextAlign.start,
+          ),
+          Text(
+            firstName,
+            style: GoogleFonts.ptSansNarrow(fontSize: 30, color: Colors.grey),
+            textAlign: TextAlign.start,
           )
         ],
       ),
