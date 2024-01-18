cb2762b1703fcac5a6fd368af9d1e0c8477a0549
Javier Prieto <jpojaviprior@gmail.com>
2024-01-18 12:31:05 +0100

    initial commit

--- Flutter/pokemon_list/lib/screen/home_screen.dart
+++ Flutter/pokemon_list/lib/screen/home_screen.dart
@@ -1,11 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:pokemon_list/models/pokemon_list/pokemon_list.dart';
 import 'package:pokemon_list/models/pokemon_list/result.dart';
+import 'package:skeletonizer/skeletonizer.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key, required this.pokemonList});
-  final pokemonList;
+  final List<Result> pokemonList;
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Pokemons'),
+        centerTitle: true,
+      ),
+      body: Skeletonizer(
+        child: ListView.builder(
+          itemCount: widget.pokemonList.length,
+          itemBuilder: (context, index){
+            return const Card(
+              
+            );
+          }),
+      ),
+    );
   }
 }
