17a7e06f2c8bc20fa22cdffd27ec0aef4521a705
Javier Prieto <jpojaviprior@gmail.com>
2024-01-19 09:16:11 +0100

    minimal changes

--- Flutter/pokemon_list/lib/widgets/pokemons_widget.dart
+++ Flutter/pokemon_list/lib/widgets/pokemons_widget.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:pokemon_list/models/pokemon_list/pokemon_list.dart';
 import 'package:pokemon_list/screen/home_screen.dart';
+import 'package:skeletonizer/skeletonizer.dart';
 
 Future<PokemonList> fetchPokemon() async {
   final response =
@@ -41,9 +42,80 @@ class PokemonListWidgetState extends State<PokemonListWidget> {
           } else if (snapshot.hasError) {
             return Text('${snapshot.error}');
           }
-          return const CircularProgressIndicator();
-        },
+          return const Skeletonizer(
+          enabled: true,
+          child: Column(
+            children: [
+              Padding(
+                padding: EdgeInsets.all(10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'hola',
+                      style: TextStyle(
+                        color: Color.fromARGB(255, 20, 46, 21),
+                      ),
+                    ),
+                    Text(
+                      'a',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    )
+                  ],
+                ),
+              ),
+              Padding(
+                  padding: EdgeInsets.all(10),
+                  child: Image(
+                    image: NetworkImage('a'),
+                    fit: BoxFit.contain,
+                    width: 200,
+                  )),
+              Padding(
+                padding: EdgeInsets.all(10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'a',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, fontSize: 20),
+                      textAlign: TextAlign.center,
+                    ),
+                    Text(
+                      'a ~ bExp',
+                      style:
+                          TextStyle(color: Colors.grey, fontSize: 15),
+                    ),
+                    Row(
+                      children: [
+                        Icon(Icons.gamepad_rounded),
+                        Text(
+                            ' a'),
+                        SizedBox(
+                          width: 5,
+                        ),
+                        Icon(Icons.monitor_weight_rounded),
+                        Text('Kg'),
+                        SizedBox(
+                          width: 5,
+                        ),
+                        Icon(Icons.height),
+                        Text(' cm'),
+                        SizedBox(
+                          width: 5,
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+                    );
+        }
       ),
     );
   }
 }
+
